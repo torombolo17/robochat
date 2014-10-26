2f1e3751464e1ececc7903003cf63497fc259f75
Ricky <ricky@ubuntu.(none)>
2014-10-25 21:13:46 -0400

    changed a path

--- roboserver.js
+++ roboserver.js
@@ -1,4 +1,4 @@
-var path = 'https://robochat.herokuapp.com/'
+var path = '/home/ricky/Documents/waterBoa/roboChat/example.html'
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
