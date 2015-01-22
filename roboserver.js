ee60cc259ef789f1ee7ae95cb1d023ae9d06986d
PedroAnibarro1 <pedroanibarro3@gmail.com>
2015-01-22 17:26:05 -0400

    Fix of the console.log

--- roboserver.js
+++ roboserver.js
@@ -30,8 +30,8 @@ io.sockets.on('connection', function (socket) {
     socket.on('sendchat', function (data) {
         var voice = unirest.get("https://api.voicerss.org/?key=116c3dfac5c3487b94014be533051b0e&src="+data+"&hl=en-us");
 
-        print(voice);
-        
+        console.log(voice);
+
         // we tell the client to execute 'updatechat' with 2 parameters
         io.sockets.emit('updatechat', socket.username, data, voice);
         db.push({name: socket.username, message: data});
