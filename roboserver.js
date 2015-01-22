dbee6886997530bb7a5d7277f8a3574eb72d86b8
PedroAnibarro1 <pedroanibarro3@gmail.com>
2015-01-22 19:10:18 -0400

    Attempt #1234

--- roboserver.js
+++ roboserver.js
@@ -28,7 +28,7 @@ io.sockets.on('connection', function (socket) {
 
     // when the client emits 'sendchat', this listens and executes
     socket.on('sendchat', function (data) {
-        var voice = unirest.get("https://api.voicerss.org/?key=116c3dfac5c3487b94014be533051b0e&src="+data+"&hl=en-us");
+        /*var voice = unirest.get("https://api.voicerss.org/?key=116c3dfac5c3487b94014be533051b0e&src="+data+"&hl=en-us");
 
         if(typeof(voice) !== 'undefined'){
             console.log("No es undefined");
@@ -36,10 +36,10 @@ io.sockets.on('connection', function (socket) {
             console.log("Es aqui donde se imprime el objeto" + voice + "!!!!!!!!");
         }else{
             console.log("Es undefined");
-        }
+        }*/
 
         // we tell the client to execute 'updatechat' with 2 parameters
-        io.sockets.emit('updatechat', socket.username, data, voice);
+        io.sockets.emit('updatechat', socket.username, data);
         db.push({name: socket.username, message: data});
     });
 
