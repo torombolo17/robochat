3b1ff6ec2a20e9ae3d778c2496c129c4f1966b2a
Jose Ricardo <j.r.delavega17@gmail.com>
2015-03-11 13:52:22 -0400

    Added a delete conversation button

--- roboserver.js
+++ roboserver.js
@@ -30,27 +30,15 @@ io.sockets.on('connection', function (socket) {
 
     // when the client emits 'sendchat', this listens and executes
     socket.on('sendchat', function (data) {
-        // url = "https://api.voicerss.org/?key=116c3dfac5c3487b94014be533051b0e&src="+data+"&hl=en-us"
-        // function httpGet(theUrl)
-        // {
-        //     xhr.open( "GET", theUrl, false );
-        //     xhr.send( null );
-        //     return xhr.responseXML;
-        // }
-        // var voice = unirest.get("https://api.voicerss.org/?key=116c3dfac5c3487b94014be533051b0e&src="+data+"&hl=en-us");
-
-        // if(typeof(voice) !== 'undefined'){
-        //     console.log("No es undefined");
-        //     console.log(typeof(voice));
-        //     console.log("Es aqui donde se imprime el objeto" + voice + "!!!!!!!!");
-        // }else{
-        //     console.log("Es undefined");
-        // }
 
         // we tell the client to execute 'updatechat' with 2 parameters
-        //voice = httpGet(url);
         io.sockets.emit('updatechat', socket.username, data);
         db.push({name: socket.username, message: data});
+    });
+
+    socket.on('deleteconv', function () {
+        db.remove();
+        io.sockets.emit('clearchat');
     });
 
     // when the client emits 'adduser', this listens and executes
